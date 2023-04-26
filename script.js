669086009ea5ca111ffd5420deb3b9a150939113
Sébastien Pereda <sebastien.pereda@gmail.com>
2023-04-26 08:43:31 +0200

    Define consts

---
+++ script.js
@@ -0,0 +1,9 @@
+const leftList = document.getElementById('left-list')
+const leftListTitle = document.getElementById('left-list-title')
+const leftInput = document.getElementById('left-list-input')
+const leftButton = document.getElementById('left-list-button')
+const rightList = document.getElementById('right-list')
+const rightListTitle = document.getElementById('right-list-title')
+const rightInput = document.getElementById('right-list-input')
+const rightButton = document.getElementById('right-list-button')
+
